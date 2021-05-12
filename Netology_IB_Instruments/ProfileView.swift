23c70458c40a4856f7b2dfe2cc4381ce2707398c
Алексей Маяков <aleksei@Mac-mini-Aleksej.local>
2021-05-12 20:49:50 +0300

    project

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Алексей Маяков on 03.05.2021.
+//
+
+import UIKit
+
+class Display: UIView {
+
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var city: UILabel!
+    
+    @IBOutlet weak var Photo: UIImageView!
+    
+    
+    @IBOutlet weak var text: UITextView!
+}
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Алексей Маяков on 03.05.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
