78bda3c030991fbb98261f77295c53bfcc7e02d0
Алексей Маяков <aleksei@Mac-mini-Aleksej.local>
2021-05-12 21:46:06 +0300

    fixes

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -2,19 +2,20 @@
 //  ProfileView.swift
 //  Netology_IB_Instruments
 //
-//  Created by Алексей Маяков on 03.05.2021.
+//  Created by Алексей Маяков on 12.05.2021.
 //
 
 import UIKit
-
-class Display: UIView {
-
+class ProfileView : UIView {
+    
+    @IBOutlet weak var image: UIImageView!
+    
     @IBOutlet weak var name: UILabel!
+
+    @IBOutlet weak var date: UILabel!
     
-    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var sity: UILabel!
     
-    @IBOutlet weak var Photo: UIImageView!
-    
-    
-    @IBOutlet weak var text: UITextView!
+
+    @IBOutlet weak var myText: UITextView!
 }
