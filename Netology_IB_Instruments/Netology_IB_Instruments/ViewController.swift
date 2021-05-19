7e8c42772014b0abf986643ea18431394f502003
Алексей Маяков <aleksei@Mac-mini-Aleksej.local>
2021-05-19 19:26:35 +0300

    New project

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Алексей Маяков on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
